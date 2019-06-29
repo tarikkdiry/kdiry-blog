2a2900fcd822445bf825eaef951e5d1246e5c8c8
Tarik Kdiry <tarik.kdiry@gmail.com>
2019-06-28 22:04:03 -0400

    dynamic page generation and contentful image rendering

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,44 +1,43 @@
 const path = require('path')
 
-module.exports.onCreateNode = ({node, actions }) => {
-    const { createNodeField } = actions
+// FOR MARKDOWN 
+// module.exports.onCreateNode = ({node, actions }) => {
+//     const { createNodeField } = actions
 
-    if (node.internal.type === 'MarkdownRemark') {
-        const slug = path.basename(node.fileAbsolutePath, '.md')
-        // console.log('@@@@@@@@@@@@@@@@@', slug) // works
+//     if (node.internal.type === 'MarkdownRemark') {
+//         const slug = path.basename(node.fileAbsolutePath, '.md')
+//         // console.log('@@@@@@@@@@@@@@@@@', slug) // works
 
-        createNodeField({
-            node,
-            name: 'slug',
-            value: slug
-        })
-    }
-}
+//         createNodeField({
+//             node,
+//             name: 'slug',
+//             value: slug
+//         })
+//     }
+// }
 
 module.exports.createPages = async ({ graphql, actions }) => {
     const { createPage } = actions
     const blogTemplate = path.resolve('./src/templates/blog.js')
     const res = await graphql(`
         query {
-            allMarkdownRemark {
+            allContentfulBlogPost {
                 edges {
                     node {
-                        fields {
-                            slug
-                        }
+                        slug
                     }
                 }
             }
         }
     `)
 
-    res.data.allMarkdownRemark.edges.forEach((edge) => {
+    res.data.allContentfulBlogPost.edges.forEach((edge) => {
         //Dont forget the edge argument, returns undefined
         createPage({
             component: blogTemplate,
-            path: `/blog/${edge.node.fields.slug}`,
+            path: `/blog/${edge.node.slug}`,
             context: {
-                slug: edge.node.fields.slug
+                slug: edge.node.slug
             }
         })
     })
