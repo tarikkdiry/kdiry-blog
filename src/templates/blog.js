aef65804516cddc1ad534d0bfed79249545b22cb
Tarik Kdiry <tarik.kdiry@gmail.com>
2019-06-28 20:36:14 -0400

    establish blog post structure using markdown approach

--- src/templates/blog.js
+++ src/templates/blog.js
@@ -1,10 +1,33 @@
+import { graphql } from 'gatsby';
 import React from 'react';
 import Layout from '../components/layout';
 
-const Blog = () => {
+export const query = graphql`
+    query (
+        $slug: String!
+    ) {
+        markdownRemark (
+            fields: {
+                slug:{
+                eq: $slug
+                }
+            }
+        ) {
+            frontmatter {
+                title   
+                date
+            }
+            html
+        }
+    }
+`
+
+const Blog = (props) => {
     return (
         <Layout>
-            This is a blog template
+            <h1>{ props.data.markdownRemark.frontmatter.title }</h1>
+            <p>{ props.data.markdownRemark.frontmatter.date }</p>
+            <div dangerouslySetInnerHTML = {{ __html: props.data.markdownRemark.html }}></div>
         </Layout>
     )
 }
