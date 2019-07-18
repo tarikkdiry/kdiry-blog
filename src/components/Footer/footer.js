ff463e419b085bff2b23478e8b15ffa4608e922d
Tarik Kdiry <tarik.kdiry@gmail.com>
2019-07-17 23:34:47 -0400

    fixed formatting issue

--- src/components/Footer/footer.js
+++ src/components/Footer/footer.js
@@ -14,7 +14,7 @@ const Footer = () => {
     `)
     return (
         <footer className={footerStyles.footer}>
-            <p>Created by {data.site.siteMetadata.author}. © 2019</p>
+            <p>C. {data.site.siteMetadata.author}. © 2019</p>
         </footer>
     )
 }
