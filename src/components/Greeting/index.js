bc79361ab04574347957231624d99ef2d3225f7c
Tarik Kdiry <tarik.kdiry@gmail.com>
2019-07-21 23:14:07 -0400

    emoji for good measure

--- src/components/Greeting/index.js
+++ src/components/Greeting/index.js
@@ -7,7 +7,7 @@ const Greeting = () => {
             <div className={greetingStyles.profile}>
                 {/* <img src={profileImage} alt="Greeting"></img> */}
             </div>
-            <h1>Hi! I'm Tarik Kdiry</h1>
+            <h1>Hi! 👋 I'm Tarik Kdiry</h1>
             <h3>Stay tuned...big things are coming</h3>
         </div>
     )
