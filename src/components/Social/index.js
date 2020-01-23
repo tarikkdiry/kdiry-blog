597a63f3b782b414c7e812cd6e3871de46f78f2e
Kdiry <tarik.kdiry@gmail.com>
2020-01-22 22:05:01 -0500

    begin social component

--- src/components/Social/index.js
+++ src/components/Social/index.js
@@ -1,10 +1,17 @@
-// import { SocialIcon } from 'react-social-icons';
-// import "../Social/index.module.scss";
+import { SocialIcon } from 'react-social-icons';
+import socialStyles from "../Social/index.module.scss";
 
-// const Social = () => {
-//     return (
+const Social = ({name, icon, color}) => {
+    return (
+        <div className={socialStyles.container}>
+            <div className={socialStyles.button}>
+                
+            </div>
+            <div className={socialStyles.icon}>
 
-//     )
-// }
+            </div>
+        </div>
+    )
+}
 
-// export default Social
+export default Social
