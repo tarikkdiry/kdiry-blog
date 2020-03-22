6afb67f462a707f1cc22921ffcc68cf3830cf60c
Tarik Kdiry <tarikkdiry@Tariks-MBP.home>
2020-03-21 20:21:01 -0400

    introduce profile component

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -4,12 +4,16 @@ import aboutStyles from "../About/index.module.scss";
 import '../../styles/global.scss';
 import Bio from "../Bio/index";
 import Timeline from "../VerticalTimeline/index";
+import Profile from '../Profile/index';
 
 const About = () => {
     return (
         <div className={aboutStyles.container}>
-            <div className={aboutStyles.educationCareer}>
+            <div className={aboutStyles.about}>
                 <Bio></Bio>
+                <div className={aboutStyles.profile}>
+                    <Profile></Profile>
+                </div>
             </div>
             <br></br>
             <div className={aboutStyles.timeline}>
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -14,20 +14,14 @@ const Header = () => {
         }
     `)
     return (
-        <header className={headerStyles.header}>
-            {/* LEFT SIDE */}
+        <header className={headerStyles.container}>
             <nav class="level">
-                <div className={headerStyles.left}>
+                <div className={headerStyles.header}>
                     <Link to="/">
                         <img src={logo} className={headerStyles.logo} alt="tk" to="/"></img>
                     </Link>
                 </div>
-            {/* RIGHT SIDE */}
-                <div className={headerStyles.right}>
-                    <ul>
-                        <p><Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to="/blog">Blog</Link></p>
-                    </ul>
-                </div>
+                <p><Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to="/blog">Blog</Link></p>
             </nav>
         </header>
     )
